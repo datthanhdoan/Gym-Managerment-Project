1753775858bb2b6380ccdbd75d91dae1f04dd9c1
Phùng Thế Khải <96609446+thekhai1801@users.noreply.github.com>
2024-03-26 10:04:20 +0700

    new UI DoanhThu

--- QLPTGym/Page_DT.xaml.cs
+++ QLPTGym/Page_DT.xaml.cs
@@ -23,32 +23,39 @@ namespace QLPTGym
     {
         private SQLiteConnection connection;
         private string databaseName = "D:/database/gym.db";
+
+        public class Hoadon
+        {
+            public int id { get; set; }
+            public string tenkh { get; set; }
+            public string sdt { get; set; }
+            public string goitap { get; set; }
+            public string ngayDK { get; set; }
+            public int tienphaitra { get; set; }
+        }
+
+        public class DoanhThu
+        {
+            public int id { get; set; }
+            public DateTime ngay { get; set; }
+            public int sotien { get; set; }
+        }
+
         public Page_DT()
         {
             InitializeComponent();
             ConnectToDatabase();
-            LoadData();
+            LoadHoadonData();
+            LoadDoanhThuData();
         }
+
         private void ConnectToDatabase()
         {
-            connection = new SQLiteConnection($"Data Source= {databaseName} ;Version=3;");
+            connection = new SQLiteConnection($"Data Source={databaseName};Version=3;");
             connection.Open();
         }
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        private void LoadData()
+        private void LoadHoadonData()
         {
             SQLiteCommand command = new SQLiteCommand("SELECT * FROM hoadon", connection);
             SQLiteDataReader reader = command.ExecuteReader();
@@ -64,23 +71,29 @@ namespace QLPTGym
                     goitap = reader.GetString(3),
                     ngayDK = reader.GetString(4),
                     tienphaitra = reader.GetInt32(5),
-
                 });
             }
             myHoadon.ItemsSource = hoadons;
             reader.Close();
-
-
         }
-        public class Hoadon
+
+        private void LoadDoanhThuData()
         {
-            public int id { get; set; }
-            public string tenkh { get; set; }
-            public string sdt { get; set; }
-            public string goitap { get; set; }
-            public string ngayDK { get; set; }
-            public int tienphaitra { get; set; }
-        }
+            SQLiteCommand command = new SQLiteCommand("SELECT * FROM doanhthu", connection);
+            SQLiteDataReader reader = command.ExecuteReader();
 
+            List<DoanhThu> doanhthus = new List<DoanhThu>();
+            while (reader.Read())
+            {
+                doanhthus.Add(new DoanhThu()
+                {
+                    id = reader.GetInt32(0),
+                    ngay = DateTime.Parse(reader.GetString(1)),
+                    sotien = reader.GetInt32(2),
+                });
+            }
+            myDoanhThu.ItemsSource = doanhthus;
+            reader.Close();
+        }
     }
 }
