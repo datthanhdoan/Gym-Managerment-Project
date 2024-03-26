6bfa112d8390eaf71cef8b8f80df579be388f881
Phùng Thế Khải <96609446+thekhai1801@users.noreply.github.com>
2024-03-26 10:15:56 +0700

    new UI page DT

--- QLPTGym/Page_DT.xaml.cs
+++ QLPTGym/Page_DT.xaml.cs
@@ -2,60 +2,30 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QLPTGym
 {
-    /// <summary>
-    /// Interaction logic for Page_DT.xaml
-    /// </summary>
     public partial class Page_DT : Page
     {
         private SQLiteConnection connection;
         private string databaseName = "D:/database/gym.db";
 
-        public class Hoadon
-        {
-            public int id { get; set; }
-            public string tenkh { get; set; }
-            public string sdt { get; set; }
-            public string goitap { get; set; }
-            public string ngayDK { get; set; }
-            public int tienphaitra { get; set; }
-        }
-
-        public class DoanhThu
-        {
-            public int id { get; set; }
-            public DateTime ngay { get; set; }
-            public int sotien { get; set; }
-        }
-
         public Page_DT()
         {
             InitializeComponent();
             ConnectToDatabase();
-            LoadHoadonData();
-            LoadDoanhThuData();
+            LoadData();
+            CalculateRevenue();
         }
 
         private void ConnectToDatabase()
         {
-            connection = new SQLiteConnection($"Data Source={databaseName};Version=3;");
+            connection = new SQLiteConnection($"Data Source= {databaseName} ;Version=3;");
             connection.Open();
         }
 
-        private void LoadHoadonData()
+        private void LoadData()
         {
             SQLiteCommand command = new SQLiteCommand("SELECT * FROM hoadon", connection);
             SQLiteDataReader reader = command.ExecuteReader();
@@ -70,30 +40,38 @@ namespace QLPTGym
                     sdt = reader.GetString(2),
                     goitap = reader.GetString(3),
                     ngayDK = reader.GetString(4),
-                    tienphaitra = reader.GetInt32(5),
+                    tienphaitra = reader.GetInt32(5)
                 });
             }
             myHoadon.ItemsSource = hoadons;
             reader.Close();
+
+            int totalCustomers = hoadons.Count;
+
+            int currentCustomers = hoadons.Count(h => !string.IsNullOrEmpty(h.ngayDK));
+
+            tbTotalCustomers.Text = $"Tổng số khách đã đăng ký: {totalCustomers}";
+            tbCurrentCustomers.Text = $"Số khách hàng hiện đang đăng ký: {currentCustomers}";
         }
 
-        private void LoadDoanhThuData()
+        private void CalculateRevenue()
         {
-            SQLiteCommand command = new SQLiteCommand("SELECT * FROM doanhthu", connection);
-            SQLiteDataReader reader = command.ExecuteReader();
-
-            List<DoanhThu> doanhthus = new List<DoanhThu>();
-            while (reader.Read())
+            List<Hoadon> hoadons = (List<Hoadon>)myHoadon.ItemsSource;
+            if (hoadons != null && hoadons.Any())
             {
-                doanhthus.Add(new DoanhThu()
-                {
-                    id = reader.GetInt32(0),
-                    ngay = DateTime.Parse(reader.GetString(1)),
-                    sotien = reader.GetInt32(2),
-                });
+                int totalRevenue = hoadons.Sum(h => h.tienphaitra);
+                tbTotalRevenue.Text = $"Tổng doanh thu: {totalRevenue} VNĐ";
             }
-            myDoanhThu.ItemsSource = doanhthus;
-            reader.Close();
+        }
+
+        public class Hoadon
+        {
+            public int id { get; set; }
+            public string tenkh { get; set; }
+            public string sdt { get; set; }
+            public string goitap { get; set; }
+            public string ngayDK { get; set; }
+            public int tienphaitra { get; set; }
         }
     }
 }
